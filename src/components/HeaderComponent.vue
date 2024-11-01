ad6c1013bf860c8043d3d5d7dbbf11bcdeb8f27f
Paloma <paloma.ditex@gmail.com>
2024-10-31 22:17:09 -0300

    Updates

--- src/components/HeaderComponent.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section class="container text-center">
-    <img src="../assets/img/International_Pokémon_logo.svg.png" alt="Logo pokemón" class="w-50" />
-    <img src="../assets/img/-Quien-es-ese-pokem-n-9-9-2024.png" alt="¿quiene es ese pokemón?" />
-  </section>
-</template>
